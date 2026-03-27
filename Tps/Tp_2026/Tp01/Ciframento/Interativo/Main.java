f484493ca96ef1c7e27e91f57c77f58ac917a268
gabriel <gabrielferreirape502@gmail.com>
2026-03-27 07:53:41 -0300

    ciframento

---
+++ Tps/Tp_2026/Tp01/Ciframento/Interativo/Main.java
@@ -0,0 +1,34 @@
+// QUESTAO 01 DO TP01
+// Autor: Gabriel Ferreira Pereira
+
+import java.util.*;
+
+class Main 
+{
+	public static void ciframento( String palavra) // Metodo que vai receber uma String e printar o caracter + 3
+	{
+		Scanner sc = new Scanner(System.in);
+		char c;
+
+		for ( int i = 0; i < palavra.length(); i++ )
+		{
+			c = palavra.charAt(i);
+			c += 3;
+
+			System.out.print("" + c);
+		}
+	}
+	
+	public static void main ( String[] args )
+	{
+		Scanner sc = new Scanner(System.in);
+		String palavra;
+		palavra = sc.nextLine();
+		
+		while ( palavra.length() != 3 || palavra.charAt(0) != 'F' || palavra.charAt(1) != 'I' || palavra.charAt(2) != 'M' )
+		{
+			ciframento(palavra);
+			palavra = sc.nextLine();
+		}
+	}
+}
