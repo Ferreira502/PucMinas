1119a921f14cbe1eb6e6b5d59011d8e0c6e8dd7c
gabriel <gabrielferreirape502@gmail.com>
2026-04-13 10:06:05 -0300

    adding list activity

--- Lab/Labs_2026/Lab04/Main.java
+++ Lab/Labs_2026/Lab04/Main.java
@@ -4,12 +4,14 @@ import java.util.*;
 class Pilha
 {
 	private int T;
-	private topo;
-	private array[];
+	private int topo;
+	private int array[];
 
 	public Pilha(int tamanho)
 	{
 		this.T = tamanho;
+		this.array = new int [T];
+		this.topo = 0;
 	} //constroi a pilha com capacidade máxima igual a tamanho
 
 	public void empilhar(int x) 
@@ -17,6 +19,7 @@ class Pilha
 		if ( topo >= array.length )
 		{
 			System.out.println("Erro");
+			return;
 		}
 
 		array[topo] = x;
@@ -26,9 +29,10 @@ class Pilha
 
 	public int desempilhar() 
 	{
-		if ( topo >= array.length || topo == 0 )
+		if ( topo == 0 )
 		{
 			System.out.println("Erro");
+			return 0;
 		}
 
 		return array[--topo];
@@ -37,7 +41,7 @@ class Pilha
 
 	public void mostrar() 
 	{
-		for ( int i = 0; i < T; i++)
+		for ( int i = topo - 1; i >= 0; i--)
 		{
 			System.out.println("" + array[i]);
 		}
@@ -46,7 +50,7 @@ class Pilha
 	public boolean pesquisar(int x)
        	{
 		boolean retorno = false;
-	 	for ( int i = 0; i < T && retorno == false; i++)
+	 	for ( int i = 0; i < topo && retorno == false; i++)
 		{
 			retorno = (array[i] == x);
 		}	
@@ -58,6 +62,62 @@ class Pilha
 
 class Main
 {
+      public static void main ( String[] args ) 
+      {
+      	System.out.println("==== Pilha ESTATICA ====");
+      	Pilha pilha = new Pilha(5);
+      	int x1, x2, x3;
 
+      	pilha.empilhar(5);
+      	pilha.empilhar(7);
+      	pilha.empilhar(8);
+      	pilha.empilhar(9);
+
+      	System.out.println("Apos insercoes(5, 7, 8, 9): ");
+      	pilha.mostrar();
+
+     	x1 = pilha.desempilhar();
+      	x2 = pilha.desempilhar();
+
+      	System.out.println("Apos remocoes (" + x1 + ", " + x2 + "):");
+      	pilha.mostrar();
+
+      	pilha.empilhar(3);
+      	pilha.empilhar(4);
+
+
+      	System.out.println("Apos insercoes(3, 4): ");
+      	pilha.mostrar();
+
+      	x1 = pilha.desempilhar();
+      	x2 = pilha.desempilhar();
+      	x3 = pilha.desempilhar();
+
+      	System.out.println("Apos remocoes (" + x1 + ", " + x2 + ", " + x3 + "):");
+      	pilha.mostrar();
+
+      	pilha.empilhar(4);
+      	pilha.empilhar(5);
+
+      	System.out.println("Apos insercoes(4, 5): ");
+      	pilha.mostrar();
+
+      	x1 = pilha.desempilhar();
+      	x2 = pilha.desempilhar();
+
+      	System.out.println("Apos remocoes (" + x1 + ", " + x2 + "):");
+      	pilha.mostrar();
+
+      	pilha.empilhar(6);
+      	pilha.empilhar(7);
+
+      	System.out.println("Apos insercoes(6, 7): ");
+      	pilha.mostrar();
+
+      	x1 = pilha.desempilhar();
+
+      	System.out.println("Apos remocao (" + x1 + "): ");
+      	pilha.mostrar();
+   }
 }
 
