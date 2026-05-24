e49870ab5dc6e4d1408048c74558a298cae537e1
Ferreira502 <gabrielferreirape502@gmail.com>
2026-05-24 15:23:23 -0300

    adding AVL tree

--- Fontes/EstruturaFlexivel/ArvoreAVL/Main.java
+++ Fontes/EstruturaFlexivel/ArvoreAVL/Main.java
@@ -1,24 +1,64 @@
-import java.util.*;
-
 class No 
 {
     private int elemento;
+    private int altura;
     private No esq, dir;
 
-    public No ( int elemento )
+    public No(int elemento) 
     {
         this.elemento = elemento;
-        this.esq.dir = null;
+        this.altura = 1;
+        this.esq = null;
+        this.dir = null;
+    }
+
+    public int getAltura()       
+    { 
+        return altura; 
+    }
+
+    public void setAltura(int h) 
+    { 
+        altura = h; 
+    }
+
+    public No getEsq()           
+    { 
+        return esq; 
+    }
+
+    public No getDir()           
+    { 
+        return dir; 
     }
 }
 
-class ArvoreAVL
+class ArvoreAVL 
 {
-    
+    private No raiz;
+
+    public ArvoreAVL() 
+    {
+        raiz = null;
+    }
+
+    int getAltura(No i) 
+    {
+        if (i == null) 
+        {
+            return 0;
+        }
+
+        return i.getAltura();
+    }
+
+    public int fatorBalanceamento(No i) 
+    {
+        if (i == null) 
+        {
+            return 0;
+        }
+        
+        return getAltura(i.getEsq()) - getAltura(i.getDir());
+    }
 }
-
-class Main
-{
-
-}
-
